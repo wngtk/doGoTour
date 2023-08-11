058800ba94219e2dbed517316f9457fffcb94313
wngtk <100271394+wngtk@users.noreply.github.com>
2023-08-11 09:22:31 +0800

    Update named_results.go

--- Basic0/named_results.go
+++ Basic0/named_results.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+// Go's return values may be named. If so, they are treated as variables **defined at the top of the function**.
+// return values 是命名为 x, y 相当于在函数开头定义了 var x, y int
 func split(sum int) (x, y int) {
 	x = sum * 4 / 9
 	y = sum - x
