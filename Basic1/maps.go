a62be9ba870c9a4b21defce330db592996702af1
wngtk <100271394+wngtk@users.noreply.github.com>
2023-08-14 14:59:28 +0800

    done with basics

---
+++ Basic1/maps.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	ret := make(map[string]int)
+	for _, w := range strings.Fields(s) {
+		ret[w]++
+	}
+	return ret
+}
+
+func main() {
+	wc.Test(WordCount)
+}
